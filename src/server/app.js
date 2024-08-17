f47e1d54573686e74e4b2a1a80fd3e4c36377db3
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-08-17 19:29:25 +0300

    setting up connection to the mongodb database

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-
+const dbConnect = require("./config/dbConnect");
 const app = express();
 
+
+//Connecting to the database
+dbConnect();
 module.exports = app;
---
+++ src/server/config/dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect('url', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true, 
+        });
+        console.log("Hey there!, You have successfully connected to the database")
+    } catch (error) {
+        console.log(`Error ${error.message}`);
+    }
+};
+
+module.exports = dbConnect;
+

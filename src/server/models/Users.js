b376f917ab223293525c0b456faf186e5b3df2b7
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-08-25 12:02:32 +0300

    enforcing password complexity rules

--- src/server/models/Users.js
+++ src/server/models/Users.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs");
+// Password validation regex
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&])[A-Za-z\d@$!%*?#&]{8,}$/;
+
 //Creating a schema
 const userSchema = mongoose.Schema({
     firstname: { type: String, required: [true, "First name is required"]},
     lastname: { type: String, required: [true, "Last name is required"]},
     email: { type: String, required: [true, "Email is required"], unique: true },
-    password: { type: String, required: [true, "Password is required"] },
+    password: { type: String, required: [true, "Password is required"],
+        validate: {
+            validator: function(v) {
+                return passwordRegex.test(v);
+            },
+            message: "Password must be at least 8 characters long, include both uppercase and lowercase letters, a number, and a special character."
+        }
+     },
     admin: { type: Boolean, default: false},
     },
     {timestamps: true}
